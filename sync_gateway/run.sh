22f2b647a671845696eade136208426e64f00ebe
Vranda2011 <vrandav@virtusa.com>
2016-04-12 17:52:44 +0530

    Update run.sh

--- sync_gateway/run.sh
+++ sync_gateway/run.sh
@@ -5,7 +5,7 @@ main() {
   set -o pipefail
 
   case "$1" in
-  *)               sync_gateway https://gist.githubusercontent.com/Vranda2011/81f015a138a1a86d6f8f7e854743d25d/raw/94f6b61f738e2fd9bfde94588e1492dcd1822751/config.json;;
+  *)               sync_gateway https://gist.githubusercontent.com/Vranda2011/81f015a138a1a86d6f8f7e854743d25d/raw/17322fd87592ef7f11c412a6700f96d669158d3b/config.json;;
   esac
 }
 

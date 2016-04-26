d521ca5efb70a16b547d986779021db07d58100d
Vranda2011 <vrandav@virtusa.com>
2016-04-26 15:43:54 +0530

    Update entrypoint.sh

--- Ionic_Sensor/entrypoint.sh
+++ Ionic_Sensor/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-git clone https://github.com/samhitha30/Sensor-app.git /appl
-cd /appl
-unzip Sensor_zip.zip
-cd /appl/sensor
+git clone https://github.com/samhitha30/Sensor-app.git /application
+cd /application
+unzip sensor.zip
+cp -r /application/sensor/platforms/android/build/outputs/apk /application
+#git init
+#git add .
+#git commit -m "First commit"
+#git remote add testt https://github.com/samhitha30/Sensor-app.git
+#git push -f testt master
+cd /application/sensor
 ionic serve -a
+
+#if(curl http://52.63.237.171:8100); then echo"Successful"; else echo"Failed"; 
+#fi

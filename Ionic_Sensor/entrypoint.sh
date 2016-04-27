7bf132c16eaf22397803d7b5bcd21e9adfbcdff0
Vranda2011 <vrandav@virtusa.com>
2016-04-27 11:49:57 +0530

    Update entrypoint.sh

--- Ionic_Sensor/entrypoint.sh
+++ Ionic_Sensor/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/samhitha30/Sensor-app.git /application
 cd /application
-unzip sensor.zip
+unzip sensorapplication.zip
 cp -r /application/sensor/platforms/android/build/outputs/apk /application
 #git init
 #git add .
